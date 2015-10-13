344ef5ca3005e7008354eebe1a45bccfde43ad63
ethrundr <ione.krenz.xxi@gmail.com>
2015-10-13 12:25:37 +0900

    Update 3 - 13.10.2015 10.25
    AJAX implementation and testing
    Tabling testing and layoutting
    Basic pre-formatting for the admin page

--- src/admin.php
+++ src/admin.php
@@ -4,12 +4,7 @@ if (!(isset($_SESSION['username'])))
 {
     header('Location: home.php');
 }
-else
-{
-    echo "Welcome to Member Page, ".$_SESSION['username'];
-}
 ?>
-<html>
 <html>
     <head>
         <title>Admin Home</title>
@@ -21,19 +16,28 @@ else
         <script type="text/javascript" src="functions.js"></script>
     </head>
     
-    <body>
-        <div id="leftmenu">
-            <table>
-                <form action="logout.php" method="GET">
-                    <tr>
-                        <td><input type="submit" value="Log Out"/></td>
-                    </tr>
-                </form>
-            </table>
-        </div>
-        <div>
-            <p id="contentArea"> Test AJAX </p>
-            <input type="button" value="Test!" onclick="loadDoc()" />
+    <body id="mainBody">
+        <div class="container">
+            <div id="leftmenu" class="column column-offset-1 ">
+                <?php echo "Welcome to Member Page, ".$_SESSION['username']; ?>
+                <table>
+                    <form action="logout.php" method="GET">
+                        <tr>
+                            <td><input type="submit" value="Log Out"/></td>
+                        </tr>
+                    </form>
+                </table>
+            </div>
+            
+            <div id="intermitten" class="column column-offset-2">
+                
+            </div>
+            
+            <div id="contentArea" class="column column-offset-3">
+                <?php echo "Test AJAX" ?><br/>
+                <input type="button" value="Test1!" onclick="loadDoc1()" /> 
+                <input type="button" value="Test2!" onclick="loadDoc2()" /> 
+            </div>
         </div>
     </body>
 </html>
--- src/functions.js
+++ src/functions.js
@@ -1,17 +1,17 @@
-function loadDoc() 
+function loadDoc0() 
 {
-//    alert("AJAX Working");
-//    var xhttp;
-//    if (window.XMLHttpRequest) 
-//    {
-//        xhttp = new XMLHttpRequest();
-//    } 
-//    
-//    else 
-//    {
-//        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//    }
-    var xhttp = new XMLHttpRequest();
+//    alert("AJAX Initiate");
+    var xhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xhttp = new XMLHttpRequest();
+    } 
+    
+    else 
+    {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+//    var xhttp = new XMLHttpRequest();
   
     xhttp.onreadystatechange = function() 
     {
@@ -21,6 +21,63 @@ function loadDoc()
         }
     }
   
-    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.open("GET", "tabling-home.php", true);
     xhttp.send();
+//    alert("AJAX Ends, text supposed to be edited");
+}
+
+function loadDoc1() 
+{
+//    alert("AJAX Initiate");
+    var xhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xhttp = new XMLHttpRequest();
+    } 
+    
+    else 
+    {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+//    var xhttp = new XMLHttpRequest();
+  
+    xhttp.onreadystatechange = function() 
+    {
+        if (xhttp.readyState == 4 && xhttp.status == 200) 
+        {
+            document.getElementById("contentArea").innerHTML = xhttp.responseText;
+        }
+    }
+  
+    xhttp.open("GET", "tabling-sample.html", true);
+    xhttp.send();
+//    alert("AJAX Ends, text supposed to be edited");
+}
+
+function loadDoc2() 
+{
+//    alert("AJAX Initiate");
+    var xhttp;
+    if (window.XMLHttpRequest) 
+    {
+        xhttp = new XMLHttpRequest();
+    } 
+    
+    else 
+    {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+//    var xhttp = new XMLHttpRequest();
+  
+    xhttp.onreadystatechange = function() 
+    {
+        if (xhttp.readyState == 4 && xhttp.status == 200) 
+        {
+            document.getElementById("contentArea").innerHTML = xhttp.responseText;
+        }
+    }
+  
+    xhttp.open("GET", "tabling-sample-2.html", true);
+    xhttp.send();
+//    alert("AJAX Ends, text supposed to be edited");
 }
